99e39de6db415be47a4c6ba2a4755cb71a2b9b4d
RiceHamburger <39089915+RiceHamburger@users.noreply.github.com>
2025-04-01 10:19:16 +0900

    重現program

--- components/GameList.vue
+++ components/GameList.vue
@@ -1,20 +1,30 @@
 <script setup>
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import SwiperCore, { Pagination, Navigation } from 'swiper'
-
-// 安裝模組
-// SwiperCore.use([Pagination, Navigation])
+const modules = [Navigation];
 
 defineProps({
   projects: {
     type: Array,
     default: () => [],
   },
+});
+
+onMounted(() => {
+  Fancybox.bind("[data-fancybox]", {
+    Toolbar: {
+      display: {
+        left: [],
+        middle: [],
+        right: ["close"],
+      },
+    },
+  });
 });
 </script>
 
@@ -28,12 +38,67 @@ defineProps({
     >
       <b-container>
         <b-row>
+          <b-col sm="6" data-aos="fade-in">
+            <b-img
+              :src="`/img/portfolio/${item.main_img}`"
+              fluid
+              :alt="item.title"
+            ></b-img>
+          </b-col>
+          <b-col sm="6" data-aos="fade-in">
+            <div class="project-name overflow">
+              <h2>{{ item.title }}</h2>
+              <ul class="listWrap">
+                <li>
+                  <span><fa icon="clock" />{{ item.create_time }}</span>
+                </li>
+              </ul>
+            </div>
+            <div class="project-info overflow">
+              <h3>Gameplay</h3>
+              <p>{{ $t(item.description) }}</p>
+            </div>
+            <div class="skills overflow">
+              <h3>Project in-charge</h3>
+              <ul class="listWrap">
+                <li v-for="charge in item.in_charge" :key="charge">
+                  <p>
+                    <fa icon="gamepad" /><span>{{ $t(charge) }}</span>
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div class="tools overflow">
+              <h3>Tools</h3>
+              <ul class="listWrap">
+                <li>
+                  <p>
+                    <fa icon="file-code" /><span>{{ item.tools }}</span>
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div class="movie overflow">
+              <h3>Video</h3>
+              <ul class="listWrap">
+                <li>
+                  <a
+                    class="video"
+                    data-fancybox
+                    :title="item.title"
+                    :href="item.video"
+                    ><fa icon="play"
+                  /></a>
+                </li>
+              </ul>
+            </div>
+          </b-col>
           <b-col sm="12">
             <Swiper
-              :loop="true"
-              :pagination="{ clickable: true }"
+              :loop="false"
               :navigation="true"
               :spaceBetween="20"
+              :modules="modules"
               class="team-carousel"
             >
               <SwiperSlide v-for="(imgArr, index) in item.imgs" :key="index">
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,9 +1,25 @@
 // filepath: /workspace/webPortfolio-nuxt3/plugins/fontawesome.js
 import { library, config } from "@fortawesome/fontawesome-svg-core";
-import { faSun, faChevronUp, faEye } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSun,
+  faChevronUp,
+  faEye,
+  faClock,
+  faGamepad,
+  faFileCode,
+  faPlay,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add([faSun, faChevronUp, faEye]);
+library.add([
+  faSun,
+  faChevronUp,
+  faEye,
+  faClock,
+  faGamepad,
+  faFileCode,
+  faPlay,
+]);
 
 config.autoAddCss = false;
 
